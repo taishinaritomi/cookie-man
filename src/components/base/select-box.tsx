6ecbb0035cd220ff17b4bdb58461cefc9ae127ec
taishinaritomi <taishinaritomi@gmail.com>
2025-09-27 20:31:18 +0900

    fix

--- src/components/base/select-box.tsx
+++ src/components/base/select-box.tsx
@@ -7,19 +7,21 @@ type Props = {
 export function SelectBox(_props: Props) {
   const { options, ...props } = _props;
 
-  <label className="relative flex items-center justify-end">
-    <div className="i-ph-tag-chevron size-3" />
-    <select
-      className="appearance-none rounded border border-slate-300 bg-white p-2 dark:border-slate-600 dark:bg-slate-800"
-      {...props}
-    >
-      {options.map((option) => {
-        return (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        );
-      })}
-    </select>
-  </label>;
+  return (
+    <label className="relative flex items-center justify-end">
+      <div className="i-ph-tag-chevron size-3" />
+      <select
+        className="appearance-none rounded border border-slate-300 bg-white p-2 dark:border-slate-600 dark:bg-slate-800"
+        {...props}
+      >
+        {options.map((option) => {
+          return (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
+    </label>
+  );
 }
